3257d31d427a06e8650a237546f388d6bed7e493
Saket Harsh <sharsh2010@gmail.com>
2022-12-05 14:08:46 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,10 +15,15 @@ then
     echo "curl is a dependency for the installation of NimbleEdge CLI. Please ensure curl is installed in your machine"
     exit 1
 fi
-curl https://github.com/NimbleEdge/nimbleEdgeContents/raw/main/nimbleshell_latest.tar.gz | tar xvz 
+curl https://github.com/NimbleEdge/nimbleEdgeContents/raw/main/nimbleshell_latest.tar.gz  --output nimbleshell_latest.tar.gz 
+
+tar -xvzf nimbleshell_latest.tar.gz -C nimbleshell_latest
 
 cp -r $currDir/nimbleshell_latest $HOME/.nimbleshell/
 rm -rf $currDir/nimbleshell_latest
 cat $HOME/.nimbleshell/README.md
 
 export PATH="$HOME/.nimbleshell/:$PATH"
+
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/NimbleEdge/nimbleEdgeContents/main/install.sh)"
